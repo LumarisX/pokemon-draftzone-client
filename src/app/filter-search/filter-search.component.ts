c75345c940d9c520cdfe1d1be049ae6813803fa7
LumarisX <65790297+LumarisX@users.noreply.github.com>
2023-12-01 22:58:24 -0800

    filter click and focusout

--- src/app/filter-search/filter-search.component.ts
+++ src/app/filter-search/filter-search.component.ts
@@ -1,44 +1,63 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BattlePokedex } from '../pokedex';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'filter-search',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './filter-search.component.html',
+  template: `
+  <div (focusout)="focusCheck($event)">
+    <input class="border" placeholder="Enter search" [(ngModel)]="query" >
+    <div *ngFor="let mon of list" (mousedown)="onClick($event)">{{mon}}</div>
+</div>
+  `,
   styleUrl: './filter-search.component.css'
 })
 export class FilterSearchComponent {
   list: string[] = [];
-  _message = "";
+  _query = "";
 
   @Input() data!: any;
 
-  set message(val: string){
-    this._message = val;
+  set query(val: string) {
+    this._query = val;
     this.filter(val);
   }
 
-  get message(){
-    return this._message;
+  get query() {
+    return this._query;
   }
 
-  filter(val:string){
-    
-    if(val){
-      val = val.toLowerCase().trim().replace(/\s+/g,'-');
+  filter(val: string) {
+
+    if (val) {
+      val = val.toLowerCase().trim().replace(/\s+/g, '-');
       let reg = new RegExp("(^|[-])" + val + ".*")
       let list: string[] = [];
-      for(let item in this.data){
-        if(reg.test(this.data[<keyof typeof this.data>item]["name"].toLowerCase().replace(/\s+/g,'-'))){
+      for (let item in this.data) {
+        if (reg.test(this.data[<keyof typeof this.data>item]["name"].toLowerCase().replace(/\s+/g, '-'))) {
           list.push(this.data[<keyof typeof this.data>item]["name"])
         }
       }
       this.list = list;
     } else {
-      this.list = []
+      this.resetList();
     }
+  }
+
+  resetList(){
+    this.list = []
+  }
+
+  onClick(event: Event){
+    const target = event.target as HTMLButtonElement;
+    if(target) this.query = target.innerText;
+    this.resetList();
+  }
+
+  focusCheck(event: Event){
+    console.log(event);
+    this.resetList();
   }
 
 }
