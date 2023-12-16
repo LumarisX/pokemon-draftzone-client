e52bd23032890d5461a37933ea47127f939f2e5f
LumarisX <65790297+LumarisX@users.noreply.github.com>
2023-12-15 22:30:27 -0800

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,18 @@ import { Draft } from './draft';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="w-full bg-slate-300 border-b-2 border-slate-400">
-    <div class="text-xl p-2">{{ title }}</div>
-    </div>
+    <header class="w-full bg-slate-300 flex border-b-2 border-slate-400">
+      <a class="text-xl p-2">DraftZone</a>
+      <div class="flex py-2 px-10 space-x-6">
+        <a routerLink="/draft">Draft</a>
+        <a>Planner</a>
+        <a>Statistics</a>
+      </div>
+      <div class="flex flex-grow justify-end p-2">Lumaris</div>
+    </header>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit {
-  title = 'DraftZone';
-  teams: Draft[] = [];
+export class AppComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-    
-  }
 }
