a73ca30242862d4f3e48db39eb46fc6532e15ad8
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-02-03 09:31:25 -0800

    removed unused functions

--- src/app/draft-overview/draft-overview.component.ts
+++ src/app/draft-overview/draft-overview.component.ts
@@ -1,84 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute, RouterModule } from '@angular/router';
-import { DraftService } from '../api/draft.service';
-import { Draft } from '../interfaces/draft';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { SpriteComponent } from '../sprite/sprite.component';
 import { CoreModule } from '../sprite/sprite.module';
-import { SpriteService } from '../sprite/sprite.service';
-import { DraftPreviewComponent } from './draft-preview/draft-preview.component';
-import { pokemonNameValidator } from '../validators/pokemon.validator';
-import { Pokemon } from '../pokemon';
 import { DraftFormComponent } from './draft-form/draft-form.component';
+import { DraftPreviewComponent } from './draft-preview/draft-preview.component';
 
 @Component({
   selector: 'app-teams',
   standalone: true,
-  imports: [CommonModule, RouterModule, CoreModule, DraftPreviewComponent, DraftFormComponent, SpriteComponent, ReactiveFormsModule],
-  templateUrl: './draft-overview.component.html'
+  imports: [
+    CommonModule,
+    RouterModule,
+    CoreModule,
+    DraftPreviewComponent,
+    DraftFormComponent,
+    SpriteComponent,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './draft-overview.component.html',
 })
-export class DraftOverviewComponent implements OnInit {
-  teams: Draft[] = [];
-  formVisible: boolean = false
-
-  constructor(private spriteService: SpriteService, private route: ActivatedRoute, private fb: FormBuilder, private draftService: DraftService) { }
-
-  draftForm = this.fb.group({
-    leagueName: [''],
-    format: ['Singles'],
-    ruleset: ['Gen9 NatDex'],
-    team: this.fb.array([
-      {
-        pokemonName: "charizardmegay" as Pokemon,
-        shiny: "false",
-      }
-    ])
-  })
-
-  default = {
-    pokemonName: '',
-    shiny: false,
-    captain: true
-  }
-
-  pokemonForm = this.fb.group({
-    pokemonName: ['', [Validators.required, pokemonNameValidator()]],
-    shiny: [false],
-    captain: [false]
-  })
-
-  ngOnInit() {
-    this.draftService.getDraftsList().subscribe(data => {
-      this.teams = <Draft[]>data;
-    });
-    this.pokemonForm.setValue(this.default)
-  }
-
-  spriteDiv(name: string) {
-    return this.spriteService.getSprite(name);
-  }
-
-  addNewPokemon() {
-    if (this.pokemonForm.valid) {
-      this.draftFormTeam.push(this.fb.control(this.pokemonForm.value))
-      this.pokemonForm.setValue(this.default)
-    }
-  }
-
-  get draftFormTeam() {
-    return this.draftForm.get('team') as FormArray
-  }
-
-  //fix depreciated 
-  onSubmit() {
-    this.draftService.newDraft(this.draftForm.value).subscribe(
-      response => {
-        console.log("Success!", response)
-        this.formVisible = false
-        this.ngOnInit()
-      },
-      error => console.error("Error!", error)
-    )
-  }
+export class DraftOverviewComponent {
+  constructor() {}
 }
