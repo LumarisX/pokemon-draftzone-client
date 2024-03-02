ab7ef95697de2b8ed1475ef119969eb247700382
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-03-02 15:11:23 -0800

    form responding

--- src/app/opponent-overview/opponent-score/opponent-score.component.ts
+++ src/app/opponent-overview/opponent-score/opponent-score.component.ts
@@ -1,19 +1,18 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
-  Form,
   FormArray,
-  FormControl,
+  FormBuilder,
   FormGroup,
   ReactiveFormsModule,
 } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { DraftService } from '../../api/draft.service';
-import { Pokemon } from '../../interfaces/draft';
 import { Matchup } from '../../interfaces/matchup';
+import { CommonModule } from '@angular/common';
 import { PokemonFormComponent } from '../../pokemon-form/pokemon-form.component';
 import { SpriteComponent } from '../../sprite/sprite.component';
 import { CoreModule } from '../../sprite/sprite.module';
+import { Pokemon } from '../../interfaces/draft';
 
 @Component({
   selector: 'opponent-form',
@@ -33,109 +32,61 @@ export class OpponentScoreComponent implements OnInit {
   matchupId: string = '';
   title: string = 'New Matchup';
   matchup!: Matchup;
+  scoreForm!: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private draftService: DraftService,
     private route: ActivatedRoute
   ) {}
 
-  get aTeamArray(): FormArray {
-    return this.scoreForm?.get('aTeam.team') as FormArray;
-  }
-
-  get bTeamArray(): FormArray {
-    return this.scoreForm?.get('bTeam.team') as FormArray;
-  }
-
-  scoreForm!: FormGroup;
-
   ngOnInit(): void {
-    this.matchupId = <string>(
-      this.route.parent!.parent!.snapshot.paramMap.get('teamid')
-    );
+    this.matchupId =
+      this.route.parent!.parent!.snapshot.paramMap.get('teamid') || '';
     this.route.queryParams.subscribe((params) => {
       if ('matchup' in params) {
         this.matchupId = JSON.parse(params['matchup']);
         this.draftService.getMatchup(this.matchupId).subscribe((data) => {
-          this.matchup = <Matchup>data;
-          let aTeamArray: FormGroup[] = [];
-          for (let pokemon of this.matchup.aTeam.team) {
-            aTeamArray.push(
-              new FormGroup({
-                kills: new FormControl(),
-                deaths: new FormControl(),
-                brought: new FormControl(),
-              })
-            );
-          }
-
-          let bTeamArray: FormGroup[] = [];
-          for (let pokemon of this.matchup.bTeam.team) {
-            bTeamArray.push(
-              new FormGroup({
-                kills: new FormControl(),
-                deaths: new FormControl(),
-                brought: new FormControl(),
-              })
-            );
-          }
-          let aTeamForm = new FormGroup({
-            pokePaste: new FormControl(''),
-            replay: new FormControl(''),
-            score: new FormControl(''),
-            team: new FormArray(aTeamArray),
-          });
-          let bTeamForm = new FormGroup({
-            pokePaste: new FormControl(''),
-            replay: new FormControl(''),
-            score: new FormControl(''),
-            team: new FormArray(bTeamArray),
-          });
-          this.scoreForm = new FormGroup({
-            aTeam: aTeamForm,
-            bTeam: bTeamForm,
-          });
+          this.matchup = data as Matchup;
+          this.initForm();
         });
       }
     });
   }
 
-  addANewPokemon(
-    index: number = this.aTeamArray.length,
-    pokemonData: Pokemon = { pid: '', name: '' }
-  ) {
-    this.aTeamArray?.insert(
-      index + 1,
-      PokemonFormComponent.addPokemonForm(pokemonData)
+  private initForm(): void {
+    this.scoreForm = this.fb.group({
+      aTeam: this.fb.group({
+        pokePaste: [''],
+        score: [''],
+        team: this.fb.array(this.initTeamArray(this.matchup.aTeam.team)),
+      }),
+      bTeam: this.fb.group({
+        pokePaste: [''],
+        score: [''],
+        team: this.fb.array(this.initTeamArray(this.matchup.bTeam.team)),
+      }),
+      replay: [''],
+    });
+  }
+
+  private initTeamArray(team: Pokemon[]): FormGroup[] {
+    return team.map((pokemon) =>
+      this.fb.group({
+        pokemon: pokemon,
+        kills: [''],
+        deaths: [''],
+        indirect: [''],
+        brought: [''],
+      })
     );
   }
 
-  addBNewPokemon(
-    index: number = this.aTeamArray.length,
-    pokemonData: Pokemon = { pid: '', name: '' }
-  ) {
-    this.bTeamArray?.insert(
-      index + 1,
-      PokemonFormComponent.addPokemonForm(pokemonData)
-    );
+  get aTeamArray(): FormArray {
+    return this.scoreForm.get('aTeam.team') as FormArray;
   }
 
-  deleteAPokemon(index: number) {
-    this.aTeamArray?.removeAt(index);
+  get bTeamArray(): FormArray {
+    return this.scoreForm.get('bTeam.team') as FormArray;
   }
-
-  deleteBPokemon(index: number) {
-    this.bTeamArray?.removeAt(index);
-  }
-
-  // //fix depreciated
-  // onSubmit() {
-  //   this.draftService.newMatchup(this.teamId, this.matchupForm.value).subscribe(
-  //     (response) => {
-  //       console.log('Success!', response);
-  //       this.reload.emit(true);
-  //     },
-  //     (error) => console.error('Error!', error)
-  //   );
-  // }
 }
