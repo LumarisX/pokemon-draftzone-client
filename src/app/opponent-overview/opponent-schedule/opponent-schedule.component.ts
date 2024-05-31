510d4c2772caeb8bc5d98769e2af6361e713eb1e
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-05-30 22:34:16 -0700

    Scheduler time convert working, not saving yet

--- src/app/opponent-overview/opponent-schedule/opponent-schedule.component.ts
+++ src/app/opponent-overview/opponent-schedule/opponent-schedule.component.ts
@@ -1,19 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import moment from 'moment-timezone';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import moment, { Moment } from 'moment-timezone';
 
 @Component({
   selector: 'opponent-schedule',
   standalone: true,
   templateUrl: './opponent-schedule.component.html',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule],
 })
 export class OpponentSchedule implements OnInit {
+  teamId: string = '';
   selectedDate: string = '';
   selectedTime: string = '';
   opponentTimeZone: string = '';
   emailReminder: boolean = true;
+  emailTime: number = 1;
   localTimeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone;
   localTimeOffset: string = moment.tz(this.localTimeZone).format('UTCZ');
   timeZones: { offset: string; name: string }[] = moment.tz
@@ -26,49 +29,62 @@ export class OpponentSchedule implements OnInit {
     }));
 
   filteredTimeZones: { offset: string; name: string }[] = this.timeZones;
-  timeZoneSearchQuery: string = '';
-  localTime: string = '';
   convertedTime: string = '';
+  convertedDate: string = '';
   timeDifference: string = '';
-  format: '12' | '24' = '12';
+  localDateTime: Moment = moment();
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
+    this.teamId = this.route.parent!.snapshot.paramMap.get('teamid') || '';
     const currentDate = new Date();
     const currentDateString = currentDate.toISOString().slice(0, 10);
     const currentTimeString = currentDate.toTimeString().slice(0, 5);
-    this.selectedDate = currentDateString; // Initialize selectedDate
-    this.selectedTime = currentTimeString; // Initialize selectedTime
+    this.selectedDate = currentDateString;
+    this.selectedTime = currentTimeString;
     this.opponentTimeZone = this.localTimeZone;
     this.updateTimes();
-    console.log(this.timeZones);
   }
 
-  updateTimes() {
-    if (this.selectedDate && this.selectedTime && this.opponentTimeZone) {
-      const selectedDateTime = `${this.selectedDate}T${this.selectedTime}`; // Combine date and time
-      const localDateTime = moment(selectedDateTime);
-      const convertedDateTime = localDateTime.clone().tz(this.opponentTimeZone);
-      if (this.format === '12') {
-        this.localTime = localDateTime.format('MM/DD/YYYY hh:mm A');
-        this.convertedTime = convertedDateTime.format('MM/DD/YYYY  hh:mm A');
-      } else {
-        this.localTime = localDateTime.format('MM/DD/YYYY HH:mm');
-        this.convertedTime = convertedDateTime.format('MM/DD/YYYY  HH:mm');
-      }
-      this.calculateTimeDifference(localDateTime);
+  updateTimes(source: 'local' | 'converted' = 'local') {
+    if (source === 'local') {
+      this.localDateTime = moment.tz(
+        `${this.selectedDate}T${this.selectedTime}`,
+        this.localTimeZone
+      );
+      const convertedDateTime = this.localDateTime
+        .clone()
+        .tz(this.opponentTimeZone);
+      this.convertedTime = convertedDateTime.format('HH:mm');
+      this.convertedDate = convertedDateTime.format('YYYY-MM-DD');
+    } else if (source === 'converted') {
+      const convertedDateTime = moment.tz(
+        `${this.convertedDate}T${this.convertedTime}`,
+        this.opponentTimeZone
+      );
+      this.localDateTime = convertedDateTime.clone().tz(this.localTimeZone);
+      this.selectedTime = this.localDateTime.format('HH:mm');
+      this.selectedDate = this.localDateTime.format('YYYY-MM-DD');
     }
+    this.calculateTimeDifference(this.localDateTime);
   }
 
-  calculateTimeDifference(localDateTime: moment.Moment) {
-    const currentTime = moment();
-    const duration = moment.duration(localDateTime.diff(currentTime));
-
-    const isPast = localDateTime.isBefore(currentTime);
-    const days = Math.floor(Math.abs(duration.asDays()));
-    const hours = Math.abs(duration.hours());
-
-    const timeDifferencePhrase = isPast ? 'ago' : 'from now';
-    this.timeDifference = `${days} days and ${hours} hours ${timeDifferencePhrase}`;
+  calculateTimeDifference(localDateTime: Moment) {
+    if (this.localDateTime.isValid()) {
+      const currentTime = moment();
+      const duration = moment.duration(localDateTime.diff(currentTime));
+      const isPast = localDateTime.isBefore(currentTime);
+      const days = Math.floor(Math.abs(duration.asDays()));
+      const hours = Math.abs(duration.hours());
+      const timeDifferencePhrase = isPast ? 'ago' : 'from now';
+      this.timeDifference =
+        days > 0
+          ? `${days} days and ${hours} hours ${timeDifferencePhrase}`
+          : `${hours} hours ${timeDifferencePhrase}`;
+    } else {
+      this.timeDifference = 'Invalid date selected';
+    }
   }
 
   filterTimeZones(event: Event) {
