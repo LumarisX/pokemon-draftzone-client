bb53ce23c7eade3c49542946f70b7819ea155d86
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-07-05 14:59:43 -0700

    fix colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth0.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   userDropdown = false;
   menuDropdown = false;
+  theme: undefined | string;
 
   constructor(public auth: AuthService) {}
 
-  checkTheme() {
-    let theme = localStorage.getItem('theme');
-    if (theme === 'shiny') {
-      return 'shiny';
-    }
-    return '';
+  ngOnInit(): void {
+    let shiny = Math.floor(Math.random() * 100);
+    this.theme = true || shiny === 0 ? 'shiny dark:darkshiny' : '';
   }
 }
