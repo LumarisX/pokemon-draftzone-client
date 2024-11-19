a6b60d8838b1fbb583b12422785ef2253811c164
LumarisX <65790297+LumarisX@users.noreply.github.com>
2024-11-19 14:48:09 -0800

    update about

--- src/app/opponent-overview/opponent-overview.component.ts
+++ src/app/opponent-overview/opponent-overview.component.ts
@@ -2,27 +2,20 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { SpriteComponent } from '../images/sprite.component';
-import { Draft } from '../interfaces/draft';
-import { OpponentTeamPreviewComponent } from './opponent-preview/opponent-preview.component';
 import { DraftService } from '../api/draft.service';
+import { DraftOverviewPath } from '../draft-overview/draft-overview-routing.module';
+import { Draft } from '../interfaces/draft';
 
 @Component({
   selector: 'opponent-overview',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-    OpponentTeamPreviewComponent,
-    SpriteComponent,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './opponent-overview.component.html',
 })
 export class OpponentOverviewComponent implements OnInit {
   draft!: Draft;
   teamId: string = '';
-
+  draftPath = DraftOverviewPath;
   constructor(
     private route: ActivatedRoute,
     private draftService: DraftService
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
