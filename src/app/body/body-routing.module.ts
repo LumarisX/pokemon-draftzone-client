182315ef422b38ae1915ce2196830dd1e8d9777f
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-01-08 08:33:34 -0800

    fix landing image

--- src/app/body/body-routing.module.ts
+++ src/app/body/body-routing.module.ts
@@ -4,12 +4,20 @@ import { AboutComponent } from '../pages/about/about.component';
 import { HomeComponent } from '../pages/homepage/homepage.component';
 import { SettingsComponent } from '../pages/settings/settings.component';
 import { PlannerComponent } from '../planner/plannner.component';
+import { BattleZonePath } from '../battle-zone/battle-zone-routing.module';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'planner', component: PlannerComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'about', component: AboutComponent },
+  {
+    path: BattleZonePath,
+    loadChildren: () =>
+      import('../battle-zone/battle-zone.module').then(
+        (m) => m.BattleZoneModule,
+      ),
+  },
   { path: '**', component: HomeComponent },
 ];
 
