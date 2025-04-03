bb3fd3fc898752ade1b1574522c8be41ab571cbd
LumarisX <65790297+LumarisX@users.noreply.github.com>
2025-04-03 11:44:00 -0700

    add tooltips, sorting, bug fixes, update news

--- src/app/drafts/matchup-overview/matchup/overview/overview.component.ts
+++ src/app/drafts/matchup-overview/matchup/overview/overview.component.ts
@@ -5,53 +5,73 @@ import { MatSortModule, Sort } from '@angular/material/sort';
 import { SpriteComponent } from '../../../../images/sprite/sprite.component';
 import { compare } from '../../../../util';
 import { Summary } from '../../matchup-interface';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BehaviorSubject } from 'rxjs';
+import { Pokemon } from '../../../../interfaces/draft';
+import { StatsTable } from '../../../../data';
 
 @Component({
   selector: 'overview',
   standalone: true,
   templateUrl: './overview.component.html',
   styleUrls: ['../../matchup.scss', './overview.component.scss'],
-  imports: [CommonModule, SpriteComponent, MatSortModule, MatIconModule],
+  imports: [
+    CommonModule,
+    SpriteComponent,
+    MatSortModule,
+    MatTooltipModule,
+    MatIconModule,
+  ],
 })
 export class OverviewComponent {
-  _teams: Summary[] = [];
+  private _teams: Summary[] = [];
+  $teams = new BehaviorSubject<
+    (Pokemon & {
+      abilities: string[];
+      index: number;
+      types: string[];
+      bst: number;
+      baseStats: StatsTable;
+    })[][]
+  >([]);
 
   @Input()
   set teams(value: Summary[]) {
     this._teams = value;
+    this.$teams.next(value.map((team) => team.team));
   }
 
   get teams() {
     return this._teams;
   }
-  constructor() {}
-
-  sortData(sort: Sort) {
-    let { direction, active } = sort;
-    if (direction === '') {
-      direction = 'asc';
-      active = 'index';
+  sortedBy: 'spe' | 'name' | null = null;
+  sortDirection: 'asc' | 'desc' | null = null;
+  sortData(sortKey: 'spe' | 'name') {
+    if (sortKey === this.sortedBy) {
+      if (this.sortDirection === 'asc') {
+        this.sortedBy = null;
+        this.sortDirection = null;
+        this.$teams.next(this.teams.map((team) => team.team));
+      } else if (this.sortDirection === 'desc') {
+        this.sortDirection = 'asc';
+        this.$teams.next(this.$teams.value.map((team) => team.reverse()));
+      }
+    } else {
+      if (sortKey === 'name') {
+        this.$teams.next(
+          this.teams.map((team) =>
+            [...team.team].sort((x, y) => x.name.localeCompare(y.name)),
+          ),
+        );
+      } else if (sortKey === 'spe') {
+        this.$teams.next(
+          this.teams.map((team) =>
+            [...team.team].sort((x, y) => y.baseStats.spe - x.baseStats.spe),
+          ),
+        );
+      }
+      this.sortedBy = sortKey;
+      this.sortDirection = 'desc';
     }
-    const isAsc = direction === 'asc';
-    for (let player of this.teams) {
-      player.team.sort((a, b) => {
-        switch (active) {
-          case 'name':
-            return compare(a.name, b.name, isAsc);
-          case 'index':
-            return compare(a.index, b.index, isAsc);
-          case 'hp':
-          case 'atk':
-          case 'def':
-          case 'spa':
-          case 'spd':
-          case 'spe':
-            return compare(a.baseStats[active], b.baseStats[active], isAsc);
-          default:
-            return 0;
-        }
-      });
-    }
-    return 0;
   }
 }
