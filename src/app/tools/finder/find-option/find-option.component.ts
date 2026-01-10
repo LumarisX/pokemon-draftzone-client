46b80228f68a4ddd8fa40d8ad13783dd9901b5a4
LumarisX <65790297+LumarisX@users.noreply.github.com>
2026-01-09 22:25:47 -0800

    fix tag field on finder

--- src/app/tools/finder/find-option/find-option.component.ts
+++ src/app/tools/finder/find-option/find-option.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -257,7 +256,7 @@ export class FindOptionComponent {
     {
       name: 'Tags',
       value: 'tags',
-      operations: ['∈', '∉'],
+      operations: ['=', '≠'],
       query: 'string',
     },
   ];
